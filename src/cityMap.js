bafdc56d363f4e0ace347752ae899fc997ba8734
Sergey Zubarev <barclays13@mail.ru>
2020-01-27 20:50:29 +0300

    change in getting coordinates

--- src/cityMap.js
+++ src/cityMap.js
@@ -17,8 +17,8 @@ const writeSideOfLight = document.querySelector('.write-side-of-light'),
     newLatitude = document.querySelector('.add-city-latitude'),
     newLongitude = document.querySelector('.add-city-longitude'),
     btnAddCity = document.querySelector('.add-city-submit'),
-    writeCitiesLocal = document.querySelector('.write-cities-local'),
-    removeLocalStorage = document.querySelector('.remove-local-storage');
+    writeCitiesLocal = document.querySelector('.write-cities-local');
+
 class CityMap {
 
     constructor(data, delimiter) {
@@ -27,17 +27,33 @@ class CityMap {
         let cleanData = dataShare.map(element => {
             return element.replace(/"/g, '').split(',');
         });
-
+        this.cleanData = cleanData;
         this.conversionData(cleanData);
+        // let = []; //список всех широт
+        // let listLongitudes = []; //список всех долгот))
+        // this.cities.map(elem => {
+        //     listLatitudes.push(Number(elem.latitude));
+        //     listLongitudes.push(Number(elem.longitude));
+        // });
+
+        // this.listLatitudes = listLatitudes;
+        // this.listLongitudes = listLongitudes;
+    }
+
+    getCoordinatesLatitudes () {
         let listLatitudes = []; //список всех широт
-        let listLongitudes = []; //список всех долгот))
         this.cities.map(elem => {
             listLatitudes.push(Number(elem.latitude));
+        });
+        return listLatitudes;
+    }
+
+    getCoordinatesLongitudes () {
+        let listLongitudes = []; //список всех широт
+        this.cities.map(elem => {
             listLongitudes.push(Number(elem.longitude));
         });
-        this.cleanData = cleanData;
-        this.listLatitudes = listLatitudes;
-        this.listLongitudes = listLongitudes;
+        return listLongitudes;
     }
 
     conversionData(data) {
@@ -52,7 +68,7 @@ class CityMap {
             const side = writeSideOfLight.value.trim().toLowerCase();
             switch (side) {
                 case 'north':
-                    const maxLatitude = Math.max(...this.listLatitudes);
+                    const maxLatitude = Math.max(...this.getCoordinatesLatitudes());
                     this.cities.forEach(element => {
                         if (maxLatitude === Number(element.latitude)) {
                             writeNameCity.textContent = `Крайний город - ${element.name}`;
@@ -61,16 +77,17 @@ class CityMap {
                     break;
 
                 case 'south':
-                    const minLatitude = Math.min(...this.listLatitudes);
+                    const minLatitude = Math.min(...this.getCoordinatesLatitudes());
                     this.cities.forEach(element => {
                         if (minLatitude === Number(element.latitude)) {
                             writeNameCity.textContent = `Крайний город - ${element.name}`;
+                            return;
                         }
                     });
                     break;
 
                 case 'east':
-                    const maxLongitude = Math.max(...this.listLongitudes);
+                    const maxLongitude = Math.max(...this.getCoordinatesLongitudes());
                     this.cities.forEach(element => {
                         if (maxLongitude === Number(element.longitude)) {
                             writeNameCity.textContent = `Крайний город - ${element.name}`;
@@ -79,7 +96,7 @@ class CityMap {
                     break;
 
                 case 'west':
-                    const minLongitude = Math.min(...this.listLongitudes);
+                    const minLongitude = Math.min(...this.getCoordinatesLongitudes());
                     this.cities.forEach(element => {
                         if (minLongitude === Number(element.longitude)) {
                             writeNameCity.textContent = `Крайний город - ${element.name}`;
@@ -94,15 +111,14 @@ class CityMap {
     }
 
     nearCity() {
-
-
         btnSearchCityNear.addEventListener('click', () => {
             if (writeLatitude.value == '' || writeLongitude.value == '') {
                 writeNameSityNear.textContent = `Введите данные`;
                 return;
             }
-            const resultLatitudes = this.listLatitudes.map(elem => writeLatitude.value - elem); // нахожу разность по широте
-            const resultLongitudes = this.listLongitudes.map(elem => writeLongitude.value - elem); // нахожу разность по долготе
+
+            const resultLatitudes = this.getCoordinatesLatitudes().map(elem => writeLatitude.value - elem);
+            const resultLongitudes = this.getCoordinatesLongitudes().map(elem => writeLongitude.value - elem);
             let distanceToСities = [];
             for (let index = 0; index < resultLatitudes.length; index++) {
                 const distance = Math.sqrt((resultLatitudes[index] ** 2) + (resultLongitudes[index] ** 2));
@@ -116,7 +132,6 @@ class CityMap {
     }
 
     stateAbbreviations() {
-
         btnSearchState.addEventListener('click', () => {
             const listStateAbbreviations = this.cities.map(item => item.state);
             let uniqueListStateAbbreviations = [];
@@ -125,6 +140,7 @@ class CityMap {
                     uniqueListStateAbbreviations.push(`${abbreviations}`);
                 }
             }
+
             let resultString = uniqueListStateAbbreviations.join(' ');
             listState.textContent = `Уникальные названия штатов: "${resultString}"`;
         });
@@ -133,28 +149,22 @@ class CityMap {
     serchState() {
 
         let resultListCities = [];
-
         readState.addEventListener('input', () => {
-
             this.cities.forEach(elem => {
-
                 if (readState.value.length < 2) {
                     resultListCities.length = 0;
                 }
-
                 if (elem.state.trim().toLowerCase() === readState.value.trim().toLowerCase()) {
                     let index = this.cities.findIndex(ind => ind === elem);
                     resultListCities.push(this.cities[index].name.trim());
                 }
             });
-
             writeNemeSities.textContent = resultListCities.join(', ');
         });
     }
 
     addCity() {
         const newCity = [];
-
         btnAddCity.addEventListener('click', () => {
             event.preventDefault();
             if (newCity.length == 1) {
@@ -167,20 +177,17 @@ class CityMap {
     }
 
     saveListCityInLocalStorage() {
-
-
         window.addEventListener("beforeunload", () => {
             let listNameCities = "";
             this.cities.map(city => {
                 listNameCities = listNameCities.concat(city.toString());
             });
 
-            localStorage.setItem('listCities', listNameCities);
+            localStorage.setItem('dataCities', listNameCities);
         });
 
         window.addEventListener("load", () => {
-            let dataShare = localStorage.getItem('listCities').split(';').filter(str => str || str.length !== 0);
-
+            let dataShare = localStorage.getItem('dataCities').split(';').filter(str => str || str.length !== 0);
             let cleanData = dataShare.map(element => {
                 return element.replace(/"/g, '').split(',');
             });
@@ -192,9 +199,7 @@ class CityMap {
                 listNameCities = listNameCities.concat(`${city.name} `);
             });
             writeCitiesLocal.textContent = listNameCities.trim();
-
         });
-
     }
 }
 
