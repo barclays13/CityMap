af3cac30080d6715dc607bc19b0832e44b0ce971
Sergey Zubarev <barclays13@mail.ru>
2020-01-27 22:33:16 +0300

    moving addEventListener to the index

--- src/cityMap.js
+++ src/cityMap.js
@@ -2,33 +2,28 @@
 import City from './City.js';
 
 const writeSideOfLight = document.querySelector('.write-side-of-light'),
-    btnSearchCity = document.querySelector('.btn-search-city'),
     writeNameCity = document.querySelector('.write-name-sity'),
     writeLatitude = document.querySelector('.write-latitude'),
     writeLongitude = document.querySelector('.write-longitude'),
-    btnSearchCityNear = document.querySelector('.btn-search-city-near'),
     writeNameSityNear = document.querySelector('.write-name-sity-near'),
     listState = document.querySelector('.list-state'),
-    btnSearchState = document.querySelector('.btn-search-state'),
     readState = document.querySelector('.read-state'),
     writeNemeSities = document.querySelector('.write-neme-sities'),
     newName = document.querySelector('.add-city-name'),
     newState = document.querySelector('.add-city-state'),
     newLatitude = document.querySelector('.add-city-latitude'),
     newLongitude = document.querySelector('.add-city-longitude'),
-    btnAddCity = document.querySelector('.add-city-submit'),
     writeCitiesLocal = document.querySelector('.write-cities-local');
 
 class CityMap {
 
     constructor(data, delimiter) {
         this.cities = [];
-        let dataShare = data.split(delimiter).filter(str => str || str.length !== 0);
-        let cleanData = dataShare.map(element => {
+        let cityString = data.split(delimiter).filter(str => str || str.length !== 0).map(element => {
             return element.replace(/"/g, '').split(',');
         });
-        this.cleanData = cleanData;
-        this.conversionData(cleanData);
+        this.cityString = cityString;
+        this.conversionData(cityString);
     }
 
     getCoordinatesLatitudes() {
@@ -55,7 +50,6 @@ class CityMap {
     }
 
     sideOfLight() {
-        btnSearchCity.addEventListener('click', () => {
             const side = writeSideOfLight.value.trim().toLowerCase();
             switch (side) {
                 case 'north':
@@ -98,101 +92,90 @@ class CityMap {
                 default:
                     writeNameCity.textContent = `Ваши введенные данные не верны, правильный формат:West, East, North, South`;
             }
-        });
     }
 
     nearCity() {
-        btnSearchCityNear.addEventListener('click', () => {
-            if (writeLatitude.value == '' || writeLongitude.value == '') {
-                writeNameSityNear.textContent = `Введите данные`;
-                return;
-            }
+        if (writeLatitude.value == '' || writeLongitude.value == '') {
+            writeNameSityNear.textContent = `Введите данные`;
+            return;
+        }
 
-            const resultLatitudes = this.getCoordinatesLatitudes().map(elem => writeLatitude.value - elem);
-            const resultLongitudes = this.getCoordinatesLongitudes().map(elem => writeLongitude.value - elem);
-            let distanceToСities = [];
-            for (let index = 0; index < resultLatitudes.length; index++) {
-                const distance = Math.sqrt((resultLatitudes[index] ** 2) + (resultLongitudes[index] ** 2));
-                distanceToСities.push(distance);
-            }
+        const resultLatitudes = this.getCoordinatesLatitudes().map(elem => writeLatitude.value - elem);
+        const resultLongitudes = this.getCoordinatesLongitudes().map(elem => writeLongitude.value - elem);
+        let distanceToСities = [];
+        for (let index = 0; index < resultLatitudes.length; index++) {
+            const distance = Math.sqrt((resultLatitudes[index] ** 2) + (resultLongitudes[index] ** 2));
+            distanceToСities.push(distance);
+        }
 
-            const minDistance = Math.min(...distanceToСities); // минимальный реззультат
-            const index = distanceToСities.findIndex(elem => elem === minDistance); //index ближайшего города
-            writeNameSityNear.textContent = `Ближайший город ${this.cities[index].name}, для ${writeLatitude.value} - широты и ${writeLongitude.value} - долготы`;
-        });
+        const minDistance = Math.min(...distanceToСities); // минимальный реззультат
+        const index = distanceToСities.findIndex(elem => elem === minDistance); //index ближайшего города
+        writeNameSityNear.textContent = `Ближайший город ${this.cities[index].name}, для ${writeLatitude.value} - широты и ${writeLongitude.value} - долготы`;
     }
 
     stateAbbreviations() {
-        btnSearchState.addEventListener('click', () => {
-            const listStateAbbreviations = this.cities.map(item => item.state);
-            let uniqueListStateAbbreviations = [];
-            for (let abbreviations of listStateAbbreviations) {
-                if (!uniqueListStateAbbreviations.includes(abbreviations)) {
-                    uniqueListStateAbbreviations.push(`${abbreviations}`);
-                }
+        const listStateAbbreviations = this.cities.map(item => item.state);
+        let uniqueListStateAbbreviations = [];
+        for (let abbreviations of listStateAbbreviations) {
+            if (!uniqueListStateAbbreviations.includes(abbreviations)) {
+                uniqueListStateAbbreviations.push(`${abbreviations}`);
             }
+        }
 
-            let resultString = uniqueListStateAbbreviations.join(' ');
-            listState.textContent = `Уникальные названия штатов: "${resultString}"`;
-        });
+        let resultString = uniqueListStateAbbreviations.join(' ');
+        listState.textContent = `Уникальные названия штатов: "${resultString}"`;
     }
 
     serchState() {
         let resultListCities = [];
-        readState.addEventListener('input', () => {
-            this.cities.forEach(elem => {
-                if (readState.value.length < 2) {
-                    resultListCities.length = 0;
-                }
+        this.cities.forEach(elem => {
+            if (readState.value.length < 2) {
+                resultListCities.length = 0;
+            }
 
-                if (elem.state.trim().toLowerCase() === readState.value.trim().toLowerCase()) {
-                    let index = this.cities.findIndex(ind => ind === elem);
-                    resultListCities.push(this.cities[index].name.trim());
-                }
-            });
-
-            writeNemeSities.textContent = resultListCities.join(', ');
+            if (elem.state.trim().toLowerCase() === readState.value.trim().toLowerCase()) {
+                let index = this.cities.findIndex(ind => ind === elem);
+                resultListCities.push(this.cities[index].name.trim());
+            }
         });
+
+        writeNemeSities.textContent = resultListCities.join(', ');
     }
 
     addCity() {
         const newCity = [];
-        btnAddCity.addEventListener('click', () => {
-            event.preventDefault();
-            if (newCity.length == 1) {
-                newCity.length = 0;
-            }
+        event.preventDefault();
+        if (newCity.length == 1) {
+            newCity.length = 0;
+        }
 
-            newCity.push([newName.value, newState.value, newLatitude.value, newLongitude.value]);
-            this.conversionData(newCity);
-        });
+        newCity.push([newName.value, newState.value, newLatitude.value, newLongitude.value]);
+        this.conversionData(newCity);
     }
 
     saveListCityInLocalStorage() {
-        window.addEventListener("beforeunload", () => {
-            let listNameCities = "";
-            this.cities.map(city => {
-                listNameCities = listNameCities.concat(city.toString());
-            });
-
-            localStorage.setItem('dataCities', listNameCities);
+        let listNameCities = "";
+        this.cities.map(city => {
+            listNameCities = listNameCities.concat(city.toString());
         });
 
-        window.addEventListener("load", () => {
-            let dataShare = localStorage.getItem('dataCities').split(';').filter(str => str || str.length !== 0);
-            let cleanData = dataShare.map(element => {
-                return element.replace(/"/g, '').split(',');
-            });
+        localStorage.setItem('dataCities', listNameCities);
+    }
 
-            this.cities = [];
-            this.conversionData(cleanData);
-            let listNameCities = "";
-            this.cities.map(city => {
-                listNameCities = listNameCities.concat(`${city.name} `);
-            });
-
-            writeCitiesLocal.textContent = listNameCities.trim();
+    getListCityInLocalStorage() {
+        let cityStrings = localStorage.getItem('dataCities').split(';').filter(str => str || str.length !== 0);
+        let cityString = cityStrings.map(element => {
+            return element.replace(/"/g, '').split(',');
         });
+
+        this.cities = [];
+        this.conversionData(cityString);
+        let listNameCities = "";
+        this.cities.map(city => {
+            listNameCities = listNameCities.concat(`${city.name} `);
+        });
+
+        writeCitiesLocal.textContent = listNameCities.trim();
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import CityMap from './cityMap.js';
 
-let citiUSA = new CityMap ('"Nashville, TN", 36.17, -86.78; "New York, NY", 40.71, -74.00; "Atlanta, GA", 33.75, -84.39; "Denver, CO", 39.74, -104.98; "Seattle, WA", 47.61, -122.33; "Los Angeles, CA", 34.05, -118.24; "Memphis, TN", 35.15, -90.05;', ';');
+let cityUSA = new CityMap ('"Nashville, TN", 36.17, -86.78; "New York, NY", 40.71, -74.00; "Atlanta, GA", 33.75, -84.39; "Denver, CO", 39.74, -104.98; "Seattle, WA", 47.61, -122.33; "Los Angeles, CA", 34.05, -118.24; "Memphis, TN", 35.15, -90.05;', ';');
 
-citiUSA.sideOfLight();
-citiUSA.nearCity();
-citiUSA.stateAbbreviations();
-citiUSA.serchState();
-citiUSA.addCity();
-citiUSA.saveListCityInLocalStorage();
+const btnAddCity = document.querySelector('.add-city-submit'),
+    btnSearchState = document.querySelector('.btn-search-state'),
+    readState = document.querySelector('.read-state'),
+    btnSearchCity = document.querySelector('.btn-search-city'),
+    btnSearchCityNear = document.querySelector('.btn-search-city-near');
+
+btnSearchCity.addEventListener('click', cityUSA.sideOfLight.bind(cityUSA));
+btnSearchCityNear.addEventListener('click', cityUSA.nearCity.bind(cityUSA));
+btnSearchState.addEventListener('click', cityUSA.stateAbbreviations.bind(cityUSA));
+readState.addEventListener('input', cityUSA.serchState.bind(cityUSA));
+btnAddCity.addEventListener('click', cityUSA.addCity.bind(cityUSA));
+window.addEventListener("beforeunload", cityUSA.saveListCityInLocalStorage.bind(cityUSA));
+window.addEventListener("load", cityUSA.getListCityInLocalStorage.bind(cityUSA));
